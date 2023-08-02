8ee61489d7dddf3dcdfa7785a5917198c771ae29
khalidtaha1999 <khalidwtaha9490@gmail.com>
2023-08-02 22:19:44 +0300

    multi auth add new columns ion users table and create seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin user',
+                'username' => 'admin_user',
+                'email' => 'admin@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ], [
+                'name' => 'user',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ], [
+                'name' => 'vendor',
+                'username' => 'vendor_user',
+                'email' => 'vendor@gmail.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ]
+        ]);
+    }
+}
