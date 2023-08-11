f46047d719164a80dbf415d7181370d0702ff21d
khalidtaha1999 <khalidwtaha9490@gmail.com>
2023-08-11 17:57:31 +0300

    Admin Panel setup and mastering

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,14 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+
+Route::put('profile/update', [ProfileController::class, 'update'])->name('profile.update');
+
+Route::put('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
